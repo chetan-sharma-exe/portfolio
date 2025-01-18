b4e1470bc3d6ce86bd198cabfc70f24c04e1e5ea
Chetan Sharma <csguru9818@gmail.com>
2025-01-18 13:35:33 +0530

    Design Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,61 @@
 import "./App.css";
 import Project from "./components/Project";
+import { CiLinkedin } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
 
 function App() {
   return (
     <div>
+      <div
+        className="about_me"
+        style={{
+          display: "flex",
+        }}
+      >
+        <div>
+          <p id="about_me_text">
+            Hi,
+            <br />I am Chetan
+          </p>
+          <p className="speciality">Front End Developer</p>
+        </div>
+        <div className="links">
+          <a href="https://github.com/chetan-sharma-exe" target="_blank">
+            <button
+              style={{
+                marginBottom: "10px",
+              }}
+              className="github_b"
+            >
+              <FaGithub className="li_text" size={25} />
+            </button>
+          </a>
+
+          <a href="www.linkedin.com/in/chetan-sharma-ab9558280" target="_blank">
+            <button className="linkedin_b">
+              <CiLinkedin className="li_text" size={25} />
+            </button>
+          </a>
+        </div>
+      </div>
       <p id="heading">My Projects</p>
       <div className="project_container">
         <Project
           projectName={"Zoogram"}
           category={"Social media"}
           link={"https://zoogram-chi.vercel.app"}
+          color={"#c3c0b4"}
         />
-        <Project projectName={"Helium"} category={"Ai chatbot"} />
+        <Project
+          projectName={"Helium"}
+          category={"Ai chatbot"}
+          color={"#dcbe3d"}
+        />
         <Project
           projectName={"Gcalculator"}
           category={"Calculator"}
           link={"https://gcalculator-eta.vercel.app"}
+          color={"#59b928"}
         />
       </div>
     </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,30 @@
-const Project = ({ projectName, category, link }) => {
+import { useState } from "react";
+
+const Project = ({ projectName, category, link, color }) => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="child">
+    <div
+      className="child"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{
+        borderColor: isHovered ? color : "#27272a", // Change color on hover
+      }}
+    >
       <div className="project_specs">
         <p className="project_name">{projectName}</p>
         <p className="category">{category}</p>
       </div>
       <div id="button_cont">
         <a href={link} target="_blank">
-          <button>Visit</button>
+          <button
+            onMouseEnter={() => setIsHovered(true)}
+            style={{
+              backgroundColor: isHovered ? color : "white", // Change color on hover
+            }}
+          >
+            Visit
+          </button>
         </a>
       </div>
     </div>
